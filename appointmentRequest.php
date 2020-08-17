ab1c0a7c359ae858cc505c232c29eb6ac56b577d
Megan Schmidt <schmidtm6@hawkmail.newpaltz.edu>
2020-08-17 11:14:44 -0400

    Finished Appointment Page and updated menu buttons

--- appointmentRequest.php
+++ appointmentRequest.php
@@ -1,16 +1,17 @@
 <?php 
 
 $month = $_POST['month'];
-$day = $_POST['day'];
-$times = $_POST['times'];
+$days = implode(",", $_POST['day']);
+$times = implode(",", $_POST['times']);
 $subject = $_POST['subject'];
 $tutorPref = $_POST['tutorPref'];
 $fullName = $_POST['fullName'];
 $email = $_POST['email'];
 $phoneNumber = $_POST['phoneNumber'];
 $confEmail = $_POST['confEmail'];
+ 
 
-function newApptReq($month, $day, $times, $subject, $tutorPref, $fullName, $email, $phoneNumber, $confEmail){
+function newApptReq($month, $days, $times, $subject, $tutorPref, $fullName, $email, $phoneNumber, $confEmail){
 	/*Database INFO*/
 	$servername="localhost";
 	$username="schmidtm6";
@@ -25,15 +26,15 @@ function newApptReq($month, $day, $times, $subject, $tutorPref, $fullName, $emai
 		die("Connection failed: " .$conn->connect_error);
 	}
 
-	$sql = "INSERT INTO TutoringWebsiteAppointmentRequest (ReqMonth, ReqDate, ReqTime, Subject, TutorPref, FullName, PhoneNumber, Email) VALUES ('$month', '$day', '$times', '$subject', '$tutorPref', '$fullName', '$phoneNumber', '$email');";
+	$sql = "INSERT INTO TutoringWebsiteAppointmentRequest (ReqMonth, ReqDate, ReqTime, Subject, TutorPref, FullName, PhoneNumber, Email) VALUES ('$month', '$days', '$times', '$subject', '$tutorPref', '$fullName', '$phoneNumber', '$email');";
 
     if ($conn->query($sql) === TRUE) {
 		echo "Thank you for your submission. ";
 		echo "Someone will contact you within 24 hours. ";
-		if ($confEmail == 'Yes') {
-			$subject = "Appointment Request Confirmation";
-			$message = "You have submitted a request for the following time(s): " + $month + $day + $times;
-			mail($email,$subject,$message);
+		if ($confEmail == "Yes") {
+			$message = 'Thank you for submitting an apppointment request. Please allow a few hours for us to confirm your appointment. Sincerely, The Math Tutors';
+			$message = wordwrap($message,70); 
+			mail($email, "Appointment Request Confirmation", $message, 'From: the.np.math.tutors@gmail.com');
 		}
     } else {
 		echo "Error: " . $sql . "<br>" . $conn->error;
@@ -42,6 +43,6 @@ function newApptReq($month, $day, $times, $subject, $tutorPref, $fullName, $emai
 	$conn->close();
 }	
 
-newApptReq($month, $day, $times, $subject, $tutorPref, $fullName, $email, $phoneNumber, $confEmail)
+newApptReq($month, $days, $times, $subject, $tutorPref, $fullName, $email, $phoneNumber, $confEmail)
 
 ?>
